21c65f4fd4d90828518dde5e9cc776765ce0b03d
Zuvendis <cgn21a@acu.edu>
2025-12-02 19:10:07 -0600

    creating the homepage

---
+++ Frontend/src/pages/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style/Homepage.css';
+
+const Homepage: React.FC = () => {
+  return (
+    <div className="homepage-container">
+      <h1 className="welcome-message">
+        Welcome to OISVS
+      </h1>
+    </div>
+  );
+};
+
+export default Homepage;
